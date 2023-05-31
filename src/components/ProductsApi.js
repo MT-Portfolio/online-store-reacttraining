32906fff17337363e6fdb87ea7ab3b0d6eb0add1
MT-Portfolio <63911815+MT-Portfolio@users.noreply.github.com>
2023-05-31 06:49:57 -0700

    Delete src directory

--- src/components/ProductsApi.js
+++
@@ -1,8 +0,0 @@
-import Axios from "axios";
-import { useState } from "react";
-
-export const getAllProducts = async () => {
-    const response = await Axios.get("https://fakestoreapi.com/products");
-    const products = response.data;
-    return products;
-};
